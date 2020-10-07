4e3b10e3091403b689865c87fa91e6ee46865842
German Dario Villafañez <german.villafanez@gmail.com>
2020-10-07 19:18:11 -0300

    Cambio en rama testing

--- prueba.java
+++ prueba.java
@@ -2,6 +2,6 @@ public class prueba
 {
     public static void main(String[] args)
     {
-        System.out.println("Esto es una prueba");
+        System.out.println("Esto es una prueba -> TESTING");
     }
 }
